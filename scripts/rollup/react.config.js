f75191be3f2fea6b1143ed295c572d53fde80cd3
slyxh2 <750772121@qq.com>
2023-10-19 18:46:29 -0400

    feat: config rollup for react package

---
+++ scripts/rollup/react.config.js
@@ -0,0 +1,43 @@
+import generatePackageJson from 'rollup-plugin-generate-package-json';
+import { getPackageJSON, resolvePkgPath, getBaseRollupPlugins } from './utils';
+
+const { name, module } = getPackageJSON('react');
+const pkgPath = resolvePkgPath(name);
+const pkgDistPath = resolvePkgPath(name, true);
+
+export default [
+    {
+        input: `${pkgPath}/${module}`,
+        output: {
+            file: `${pkgDistPath}/index.js`,
+            name: 'index.js',
+            format: 'umd'
+        },
+        plugins: [...getBaseRollupPlugins(), generatePackageJson({
+            inputFolder: pkgPath,
+            outputFolder: pkgDistPath,
+            baseContents: ({ name, description, version }) => ({
+                name,
+                description,
+                version,
+                main: 'index.js'
+            })
+        })]
+    },
+    {
+        input: `${pkgPath}/src/jsx.ts`,
+        output: [
+            {
+                file: `${pkgDistPath}/jsx-runtime.js`,
+                name: 'jsx-runtime.js',
+                format: 'umd'
+            },
+            {
+                file: `${pkgDistPath}/jsx-dev-runtime.js`,
+                name: 'jsx-runtime.js',
+                format: 'umd'
+            }
+        ],
+        plugins: getBaseRollupPlugins()
+    }
+]
