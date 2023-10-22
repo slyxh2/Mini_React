419d358815cd0e55e4d9900ae311e2a4ed3d3b6c
slyxh2 <750772121@qq.com>
2023-10-22 15:40:50 -0400

    fix: ts generics fix

--- packages/react-reconciler/src/ReactFiberReconciler.ts
+++ packages/react-reconciler/src/ReactFiberReconciler.ts
@@ -2,31 +2,31 @@ import { Container } from 'hostConfig';
 import { FiberNode, FiberRootNode } from './ReactFiber';
 import { HostRoot } from './ReactWorkTags';
 import {
-    UpdateQueue,
-    createUpdate,
-    createUpdateQueue,
-    enqueUpdate
+	UpdateQueue,
+	createUpdate,
+	createUpdateQueue,
+	enqueUpdate
 } from './ReactFiberUpdateQueue';
 import { ReactElementType } from 'shared/ReactTypes';
 import { scheduleUpdateOnFiber } from './ReactFiberWorkLoop';
 
 export function createContainer(container: Container): FiberRootNode {
-    const hostRootFiber = new FiberNode(HostRoot, {}, null);
-    const root = new FiberRootNode(container, hostRootFiber);
-    hostRootFiber.updateQueue = createUpdateQueue();
-    return root;
+	const hostRootFiber = new FiberNode(HostRoot, {}, null);
+	const root = new FiberRootNode(container, hostRootFiber);
+	hostRootFiber.updateQueue = createUpdateQueue();
+	return root;
 }
 
 export function updateContainer(
-    element: ReactElementType | null,
-    root: FiberRootNode
+	element: ReactElementType | null,
+	root: FiberRootNode
 ) {
-    const hostRootFiber = root.current;
-    const update = createUpdate<ReactElementType | null>(element);
-    enqueUpdate<ReactElementType | null>(
-        hostRootFiber.updateQueue as UpdateQueue<ReactElementType | null>,
-        update
-    );
-    scheduleUpdateOnFiber(hostRootFiber);
-    return element;
+	const hostRootFiber = root.current;
+	const update = createUpdate<ReactElementType | null>(element);
+	enqueUpdate<ReactElementType | null>(
+		hostRootFiber.updateQueue as UpdateQueue<ReactElementType | null>,
+		update
+	);
+	scheduleUpdateOnFiber(hostRootFiber);
+	return element;
 }
